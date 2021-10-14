2f6edcb37095cb1adc2c93c3b89694cbb4370d72
swh <sungwoo166@gmail.com>
2021-10-14 16:40:33 +0900

    날짜로 검색
     ㅆ까벌

--- CrawlWebsite/crawlInterpark.py
+++ CrawlWebsite/crawlInterpark.py
@@ -1,10 +1,9 @@
-import datetime
-from time import sleep
 
+from time import sleep
 import requests
 import time
 from bs4 import BeautifulSoup
-
+import datetime as dt
 import userinfo, get_browser
 browser = get_browser.get_browser()
 
@@ -16,14 +15,22 @@ browser.find_element_by_xpath('//*[@id="frmMemberLogin"]/div[1]/button').click()
 
 # #데이터 가져오기
 
-now = datetime.datetime.now()
-year = 0
-month = 0
-day = 0
+# NOW
+now = dt.datetime.now()
+#1주일전
+week = dt.datetime.now() -dt.timedelta(weeks=1)
 
 # #데이터 가져오기
 sleep(2)
-browser.get('https://seller.interpark.com/api/orders/acknowledge?orderSendStep=acknowledge&orderStatus=50&detailedSearchType=&detailedSearchValue=&searchPeriodType=orderDate&startDate=2021-09-13T15%3A00%3A00Z&endDate=2021-10-14T14%3A59%3A00Z&page=1&size=30')
+url = 'https://seller.interpark.com/api/orders/acknowledge?orderSendStep=acknowledge&orderStatus=50&detailedSearchType=&detailedSearchValue=&searchPeriodType=orderDate&startDate=2021-09-13T15%3A00%3A00Z&endDate=2021-10-14T14%3A59%3A00Z&page=1&size=30'
+url = 'https://seller.interpark.com/api/orders/acknowledge?orderSendStep=acknowledge&orderStatus=50&detailedSearchType=&detailedSearchValue=&searchPeriodType=orderDate&startDate='
+url += week.strftime('%Y-%m-%d')
+url += 'T15%3A00%3A00Z&endDate='
+url += now.strftime('%Y-%m-%d')
+url += 'T14%3A59%3A00Z&page=1&size=30'
+print(url)
+
+browser.get(url)
 sleep(2)
 
 html = browser.page_source
