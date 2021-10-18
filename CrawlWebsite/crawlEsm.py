dc5230888c3de7ab1d31d735a3b0c935fc7ca4e7
swh <sungwoo166@gmail.com>
2021-10-18 19:01:17 +0900

    esm
    .+

--- CrawlWebsite/crawlEsm.py
+++ CrawlWebsite/crawlEsm.py
@@ -1,12 +1,8 @@
 from time import sleep
-
-import pandas as pd
 from bs4 import BeautifulSoup
 import userinfo, get_browser
 import pandas as pd
 
-
-# df = pd.DataFrame()
 def crawlEsm():
     # 로그인
     try:
@@ -17,9 +13,9 @@ def crawlEsm():
         browser.find_element_by_id("btnLogOn").click()
         # browser.get('https://www.esmplus.com/Escrow/Order/NewOrder?type=N2&menuCode=TDM105') #새주문
         browser.get('https://www.esmplus.com/Escrow/Delivery/Sending?status=1050&type=N&menuCode=TDM111') #완료된주문
-
     except:
         print("로그인 중 중에러")
+
 
     # 브라우저 html 받기
     html = browser.page_source
@@ -28,7 +24,7 @@ def crawlEsm():
 
     # 데이터가져오기
     data = soup.select_one('#dataGrid > table > tbody')
-    tr = data.findAll("tr")  # tr이 주문자 개수
+    tr = data.findAll("tr") # tr이 주문자 개수
     length = len(tr)  # 테이블의 사람들 개수
 
     sleep(2)
@@ -39,25 +35,31 @@ def crawlEsm():
         data = soup.select_one('#dataGrid > table > tbody') #tbody
         tbody = soup.select_one('tbody.sb-grid-results') #결과
         get_info(tbody, length,soup)
-
+    browser.close()
 
 
 def get_info(tbody, length, soup):
-    testList = [0] * 70
-    sleep(3)
+    testList = [[0]*62 for _ in range(62)]*length # testList = [[0]*62] * length
     tbody = soup.select_one('tbody.sb-grid-results')
-    # info = tbody.select('tr:nth-child(1)>td')
     cnt = 1 #tr 순서 선택
-    sleep(3)
-    #     info = tbody.select('tr:nth-child(%d)>td' % cnt)
+    sleep(2)
     for i in range(length):
         info = tbody.select('tr:nth-child(%d)>td' % cnt) # 첫번째 tr 선택
-        count = 0 #배열에 넣기 위한 count 증가
+        jcount = 0 #배열에 넣기 위한 count 증가
         cnt += 1  # 다음 tr 을위해 증가
         for j in info:
-            testList[count] = j.get_text() + ',' #배열에 삽입
-            print(testList[count]) #리스트에 들어간 value들 표시
-            count += 1
-        # df.iloc[count] = testList
-        print("-" * 100)
-    # print(df)
+            testList[i][jcount] = j.get_text() + ',' #배열에 삽입
+            print(testList[i][jcount]) #리스트에 들어간 value들 표시
+            jcount += 1
+        print("-"*100)
+    for i in range(length): #2차원 배열 체크용
+        for j in range(62):
+            print(i,j)
+            print(testList[i][j])
+        print("-"*100)
+
+    df = pd.DataFrame.from_records(testList)
+    print(df)
+    print("-" * 100)
+
+
