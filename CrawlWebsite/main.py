539876c87de024383e83575244da842d3cbd2c60
SWH <sungwoo166@gmail.com>
2021-11-08 19:36:24 +0900

    네이버 수정
    .

--- CrawlWebsite/main.py
+++ CrawlWebsite/main.py
@@ -2,12 +2,12 @@ from crawlwarehouse import crawlNaver
 from crawlwarehouse import crawlInterpark, crawlEsm,crawlCoupang,crawlNaver,crawlTmon,crawlWemap
 import crawlEleven
 # crawlEleven.crawlEleven()
-# crawlNaver.crawlNaver()
-crawlEsm.crawlEsm()
-crawlInterpark.crawlInterpark()
-crawlWemap.crawlWemap()
-crawlCoupang.crawlCoupang()
-crawlTmon.crawlTmon()
+crawlNaver.crawlNaver()
+# crawlEsm.crawlEsm()
+# crawlInterpark.crawlInterpark()
+# crawlWemap.crawlWemap()
+# crawlCoupang.crawlCoupang()
+# crawlTmon.crawlTmon()
 
 import requests
 # url = '/v2/providers/openapi/apis/api/v4/vendors/{vendorId}/ordersheets'
