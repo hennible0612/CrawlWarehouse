a879e1202a92ae7d4c775dc8ea3e905e877d42cd
SWH <sungwoo166@gmail.com>
2021-11-13 21:35:12 +0900

    ESM DF to sqlite3
    .

--- testDB/esm/management/commands/add_data.py
+++ testDB/esm/management/commands/add_data.py
@@ -1,7 +1,16 @@
+import pandas as pd
 from django.core.management.base import BaseCommand
+from esm.models import Esm
+from sqlalchemy import create_engine
 
 class Command(BaseCommand):
     help = 'A command to add data from and csv file to database'
 
     def handle(self, *args, **options):
-        print("hello world")
+        csv_file = 'esm.csv'
+        df = pd.read_csv(csv_file)
+
+        engine = create_engine('sqlite:///db.sqlite3')
+
+        df.to_sql(Esm._meta.db_table, con=engine, if_exists='replace',index=True )
+
