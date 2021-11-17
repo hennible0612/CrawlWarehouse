adfc2400e2ac0e8ae11c38609df497944316b4c3
SWH <sungwoo166@gmail.com>
2021-11-17 20:05:11 +0900

    모델 수정
    .

--- mallDB/mallboard/management/commands/add_data.py
+++ mallDB/mallboard/management/commands/add_data.py
@@ -14,18 +14,18 @@ class Command(BaseCommand):
             reader = csv.DictReader(csvfile)
             for row in reader:
                 print(row)
-                p = Coupang(deliveredDate=row.deliveredDate, separated=row.separated,remoteAreaCharge = row.remoteAreaCharge,
-                shippingNum= row.shippingNum, parentShipmentBoxId =row.parentShipmentBoxId, orderNum=row.orderNum,
-                            shippingBundleId =row.shippingBundleId, customerIdNum=row.customerIdNum, customerName=row.customerName,
-                            shippingFee = row.shippingFee, invoiceNumber=row.invoiceNumber,deliveryStatusCode=row.deliveryStatusCode,
-                            postCode = row.postCode, orderRequirements=row.orderRequirements, recipientAddressOne=row.recipientAddressOne,
-                            recipientAddressTwo = row.recipientAddressTwo, customerCellNum2 = row.customerCellNum2, customerPhoneNum =row.customerPhoneNum,
-                            recipientName = row.recipientName, deliverCode = row.deliverCode, deliverName=row.deliverName, trackingUrl=row.trackingUrl,
-                            pcpCourier = row.pcpCourier, pcpContractType = row.pcpContractType, pcpDelivering = row.pcpDelivering, pcpDeliverable=row.pcpDeliverable,
-                            productShipmentType = row.productShipmentType,errorType=row.errorType, errorMessage =row.errorMessage, ableSplitShipping = row.ableSplitShipping,
-                            updatedPlannedShippingDate = row.updatedPlannedShippingDate, coupangFCSellerSendTo =row.coupangFCSellerSendTo, logistics=row.logistics,
-                            recipientAddress = row.recipientAddress, giveSecondBool = row.giveSecondBool, remoteAreaDelivery=row.remoteAreaDelivery,
-                            hasMultipleItems = row.hasMultipleItems, confirmed = row.confirmed, createdAt = row.createdAt, modifiedAt =row.modifiedAt,
-                            customerCellNum1 = row.customerCellNum1, customerCellNum = row.customerCellNum, safeNumber=row.safeNumber, safeNumberSrl = row.safeNumberSrl
+                p = Coupang(deliveredDate=row["deliveredDate"], separated=row["separated"],remoteAreaCharge = row["remoteAreaCharge"],
+                            shippingNum= row["shippingNum"], parentShipmentBoxId =row["parentShipmentBoxId"], orderNum=row["orderNum"],
+                            shippingBundleId =row["shippingBundleId"], customerIdNum=row["customerIdNum"], customerName=row["customerName"],
+                            shippingFee = row["shippingFee"], invoiceNumber=row["invoiceNumber"],deliveryStatusCode=row["deliveryStatusCode"],
+                            postCode = row["postCode"], orderRequirements=row["orderRequirements"], recipientAddressOne=row["recipientAddressOne"],
+                            recipientAddressTwo = row["recipientAddressTwo"], customerCellNum2 = row["customerCellNum2"], customerPhoneNum =row["customerPhoneNum"],
+                            recipientName = row["recipientName"], deliverCode = row["deliverCode"], deliverName=row["deliverName"], trackingUrl=row["trackingUrl"],
+                            pcpCourier = row["pcpCourier"], pcpContractType = row["pcpContractType"], pcpDelivering = row["pcpDelivering"], pcpDeliverable=row["pcpDeliverable"],
+                            productShipmentType = row["productShipmentType"],errorType=row["errorType"], errorMessage =row["errorMessage"], ableSplitShipping = row["ableSplitShipping"],
+                            updatedPlannedShippingDate = row["updatedPlannedShippingDate"], coupangFCSellerSendTo =row["coupangFCSellerSendTo"], logistics=row["logistics"],
+                            recipientAddress = row["recipientAddress"], giveSecondBool = row["giveSecondBool"], remoteAreaDelivery=row["remoteAreaDelivery"],
+                            hasMultipleItems = row["hasMultipleItems"], confirmed = row["confirmed"], createdAt = row["createdAt"], modifiedAt =row["modifiedAt"],
+                            customerCellNum1 = row["customerCellNum1"], customerCellNum = row["customerCellNum"], safeNumber=row["safeNumber"], safeNumberSrl = row["safeNumberSrl"]
                             )
                 p.save()
