92c2d441a806d6492c2fa91d0c7b4e19790e1930
SWH <sungwoo166@gmail.com>
2021-11-26 18:00:46 +0900

    다듬기
    .

--- mallDB/mallboard/views.py
+++ mallDB/mallboard/views.py
@@ -102,47 +102,7 @@ import csv
 
 @login_required(login_url='common:login')
 def getneworder(request):
-    # addInterpark
-    # crawlInterpark.crawlInterpark()
-    # with open('interpark.csv', encoding='UTF8') as csvfile:
-    #     reader = csv.DictReader(csvfile)
-    #     for row in reader:
-    #         p = Interpark(orderDate=row["orderDate"] ,recipientAddress=row["recipientAddress"]
-    #                       ,serviceUsageFee=["serviceUsageFee"] ,callsafeYn=["callsafeYn"],
-    #                       clmreqCnt=row["clmreqCnt"] ,crmCouponDiscountAmount=row["crmCouponDiscountAmount"]
-    #                       ,currentOrdclmprdStat=row["currentOrdclmprdStat"],
-    #                       currentOrdclmprdStatNm=row["currentOrdclmprdStatNm"] ,shippingFee=["shippingFee"]
-    #                       ,orderAmount1=["orderAmount1"] ,orderType=["orderType"],
-    #                       delvAttbt=row["delvAttbt"], delvAttbtNm=row["delvAttbtNm"]
-    #                       ,orderRequirements=row["orderRequirements"] ,delvWhDt=["delvWhDt"] ,delvwhOrdNo=["delvwhOrdNo"],
-    #                       delvwhPreDate=row["delvwhPreDate"] ,dtlAddr=row["dtlAddr"] ,dtlAddrDoro=row["dtlAddrDoro"]
-    #                       ,couponAppliedAmount=["couponAppliedAmount"],
-    #                       discountAmount=row["discountAmount"] ,expectedSellingFee=row["expectedSellingFee"]
-    #                       ,expectedSettlementAmt=row["expectedSettlementAmt"],
-    #                       sellerCode=row["sellerCode"] ,externalPrdNo=row["externalPrdNo"] ,feeRate=row["feeRate"]
-    #                       ,hdelvMafcEntrNoDefault=row["hdelvMafcEntrNoDefault"],
-    #                       inpkCouponDiscountAmount=row["inpkCouponDiscountAmount"] ,interestFreeFee=row["interestFreeFee"]
-    #                       ,ipointDcAmt=row["ipointDcAmt"] ,ipointFee=row["ipointFee"],
-    #                       manufacture=row["manufacture"] ,marginRate=row["marginRate"]
-    #                       ,modDelvwhPreDate=row["modDelvwhPreDate"] ,modDelvwhPreDateYn=row["modDelvwhPreDateYn"],
-    #                       odpDtlAddr=row["odpDtlAddr"], odpDtlAddrDoro=row["odpDtlAddrDoro"] ,odpZipAddr=row["odpZipAddr"]
-    #                       ,odpZipAddrDoro=row["odpZipAddrDoro"],
-    #                       odpZipNo=row["odpZipNo"] ,odpZipNoDoro=row["odpZipNoDoro"] ,optFnm=row["optFnm"]
-    #                       ,optParentSeq=row["optParentSeq"] ,optPrdTp=row["optPrdTp"],
-    #                       optPrdTpNm=row["optPrdTpNm"] ,orderAmount=row["orderAmount"] ,ordEngnm=row["ordEngnm"]
-    #                       ,customerId=["customerId"] ,customerCellNum=row["customerCellNum"],
-    #                       customerName=row["customerName"] ,ordSeq=row["ordSeq"] ,customerCellNum2=row["customerCellNum2"]
-    #                       ,ordclmDts=row["ordclmDts"] ,ordclmNo=row["ordclmNo"],
-    #                       paidDate=row["paidDate"], parentClaimRequestYn=row["parentClaimRequestYn"]
-    #                       ,productNum=row["productNum"] ,paymentMethod=row["paymentMethod"],
-    #                       productName=row["productName"] ,productNum1=row["productNum1"]
-    #                       ,preUseUnitcost=row["preUseUnitcost"] ,unitPrice=row["unitPrice"] ,unitSold=row["unitSold"],
-    #                       customerPhoneNum=row["customerPhoneNum"] ,recipientName=row["recipientName"], recipientCellNum=row["recipientCellNum"] ,recipientIdNum=row["recipientIdNum"],
-    #                       residentNo=row["residentNo"] ,rowNum=row["rowNum"] ,unitSoldTotal=row["unitSoldTotal"]
-    #                       ,specialYn=row["specialYn"] ,todayDelvYn=row["todayDelvYn"],
-    #                       totalFee=row["totalFee"] ,totalPrice=row["totalPrice"] ,totalRecCnt=row["totalRecCnt"], zipAddr=row["zipAddr"] ,zipAddrDoro=row["zipAddrDoro"] ,postCode=row["postCode"],
-    #                       zipNo=row["zipNo"] ,zipNoDoro=row["zipNoDoro"]
-    #                       )
-    #         # p.save()
+    print(request)
+    # addInterpark.addInterpark()
 
     return interpark(request)
